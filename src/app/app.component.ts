c49e02c3aec3b3fe7bb1584b7d8767053ca99d40
nathanielpascual <nath_pascual@yahoo.com>
2019-01-03 00:26:38 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { formControlBinding } from '@angular/forms/src/directives/ng_model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+
+  signUpForm : FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username':new FormControl(null),
+      'email': new FormControl(null),
+      'gender' : new FormControl('male')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+}
